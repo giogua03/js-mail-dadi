cad38a7fe01a01d401a04b0a90d9d8868550734b
giogua03 <giorgiogua03@gmail.com>
2022-10-25 17:24:55 +0200

    dadi nn funz

--- scrypt.js
+++ scrypt.js
@@ -2,7 +2,7 @@
 const eleEmailList= ['giorgio@gmail.com', 'gennaro@gmail.com', 'cloe@gmail.com', 'clara@gmail.com', 'boolen@gmail.com', 'iosonogroot@gmail.com'];
 const Mail = document.getElementById('email');
 const eleRisultato = document.getElementById('risultato');
-const eleClick = document.getElementById("bottone1");
+const eleClick = document.getElementById('bottone1');
 
    
 eleClick.addEventListener('click', 
@@ -11,5 +11,21 @@ eleClick.addEventListener('click',
             eleRisultato.innerHTML = 'EMAIL CORRETTA'
         }else{
             eleRisultato.innerHTML = 'EMAIL SBAGLIATA RIPROVA'
+        }   
+});
+
+const eleRisultatoDadi = document.getElementById('risultato2');
+const eleClickDadi = document.getElementById('bottone2');
+const eleDadoGiocatore = document.getElementById('dado1')
+ eleDadoGiocatore = Math.floor(Math.random() * 7);
+ const eleDadoCompiuter = document.getElementById('dado2')
+ eleDadoCompiuter = Math.floor(Math.random() * 7);
+
+eleClickDadi.addEventListener('click', 
+    function() {
+        if(eleDadoGiocatore > eleDadoCompiuter){
+            eleRisultatoDadi.innerHTML = 'HA VINTO IL PLAYER'
+        }else{
+            eleRisultatoDadi.innerHTML = 'HA VINTO IL COMPIUTER'
         }   
 });
